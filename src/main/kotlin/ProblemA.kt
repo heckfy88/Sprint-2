f905beb710645b24c1aad31b3e516cbad836b3d9
heckfy88 <54860448+heckfy88@users.noreply.github.com>
2021-08-25 14:17:54 +0300

    Update ProblemA.kt

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -3,7 +3,6 @@ class ProblemA {
     fun main() {
 
         var entryAmount: Int = readInt() // 1 <= количество запросов <= 1000
-        val outputArray = ArrayList<Long>()
 
         while (entryAmount-- > 0) {
             val (evenJumpInc, oddJumpInc, jumpAmount) = readLine()!!.trim().split("\\s+".toRegex()).map(String::toLong)
